99dc7bf99ea3a5e65f7fcb8c3d8e860817aa2be0
Yegor L <mmarf@inbox.ru>
2022-08-11 23:03:20 +0600

    completed

---
+++ Task_006_divide/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Greetings user! Let's see if your number is even.");
+Console.Write("Enter your number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if ( x % 2 == 0)
+{
+    Console.WriteLine("Yes, it is even.");
+}
+else
+{
+    Console.WriteLine("No, it isn't even.");
+}
