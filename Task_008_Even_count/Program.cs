4a1be229a1592089c241ae6b1c7b8dd4b0a5cbf2
Yegor L <mmarf@inbox.ru>
2022-08-11 23:09:15 +0600

    completed

---
+++ Task_008_Even_count/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Greetings user! Let's see all even numbers before your number.");
+Console.Write("Enter your number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+Console.Write("So here are your even numbers: ");
+while(count < x)
+{
+    Console.Write(" ");
+    Console.Write(count);
+    count = count + 2;
+}
