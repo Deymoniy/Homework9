c341ef6dc99dd2e40d34c7369d21c49dfcb20f1e
Yegor L <mmarf@inbox.ru>
2022-08-11 22:47:06 +0600

    completed

---
+++ Task_004_out_of_3/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Greetings user! Let's compare three numbers.");
+Console.Write("Enter your first number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter your second number: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter your third number: ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+if (x > y)
+{
+    Console.Write("The largest number is:  ");
+    Console.WriteLine(x);
+}
+ else
+{ if(y > z) {
+   Console.Write("The largest number is:  ");
+   Console.WriteLine(y);
+    }
+    else
+    {
+    Console.Write("The largest number is:  ");
+    Console.WriteLine(z);
+    }
+}
