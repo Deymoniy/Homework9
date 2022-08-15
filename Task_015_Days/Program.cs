92c1e3d04080ecee1cab76a755e94ab9d71fb31b
Yegor L <mmarf@inbox.ru>
2022-08-15 20:33:01 +0600

    Beauty

--- Task_015_Days/Program.cs
+++ Task_015_Days/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Greetings user! Let's check if that's a weekend day.");
+Console.Write("Enter a number of the day: ");
+int input = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"So your number is {input}. Got it. Gimme a second to think.");
+string[]week = {"Monday","Tuesday","Wednesday", "Thirsday", "Friday", "Saturday", "Sunday"};
+if ((input < 8) && (input > 0))
+{
+    if(input <= 5) Console.WriteLine($"Your day is {week[input - 1]}. And it's not a weekend.");
+    else Console.WriteLine($"Your day is {week[input]}. And it is a weekend. Yay!");
+}
+else
+{
+    Console.WriteLine($"Number {input} is wrong. Please enter number from 1 to 7.");
+}
