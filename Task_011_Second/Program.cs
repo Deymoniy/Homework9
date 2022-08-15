7bb15c026246af660fb40be928f16e5859f4fcd5
Yegor L <mmarf@inbox.ru>
2022-08-15 22:25:57 +0600

    final

--- Task_011_Second/Program.cs
+++ Task_011_Second/Program.cs
@@ -3,7 +3,7 @@ Console.WriteLine("Greetings user! Let's check what is the second figure in your
 Console.Write("Enter your three - digit number: ");
 int input = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"So your number is {input}. Got it. Gimme a second to think.");
-if ((input < 1000) & (input > 99))
+if ((input < 1000) && (input > 99))
     {
     int x = input / 10;
     int result = x % 10;
