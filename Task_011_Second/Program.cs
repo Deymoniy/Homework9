8c3392b40bd76d3efaa89289bf91a411bcaf3402
Yegor L <mmarf@inbox.ru>
2022-08-15 20:19:08 +0600

    Finished

---
+++ Task_011_Second/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Greetings user! Let's check what is the second figure in your number.");
+Console.Write("Enter your three - digit number: ");
+int input = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"So your number is {input}. Got it. Gimme a second to think.");
+if ((input < 1000) & (input > 99))
+    {
+    int x = input / 10;
+    int result = x % 10;
+    Console.WriteLine($"There is your second figure {result}");
+    }
+else
+    {
+    Console.WriteLine("Wrong number");
+    }
