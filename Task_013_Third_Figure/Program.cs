9a61b359130110207ce81e9e0c8ac73c79f82d66
Yegor L <mmarf@inbox.ru>
2022-08-15 22:19:18 +0600

    Final

--- Task_013_Third_Figure/Program.cs
+++ Task_013_Third_Figure/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Greetings user! Let's check what is the third figure in your number.");
+Console.Write("Enter your number: ");
+int user = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"So your number is {user}. Got it. Gimme a second to think.");
+if (user > 100) Console.WriteLine($"The second figure is {user.ToString()[2] - '0'}.");
+else Console.WriteLine("Whooops! There isn't any in here");
