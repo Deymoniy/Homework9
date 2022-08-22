97809f07e7821d4c56470a36bf96244008fa95c1
Yegor L <mmarf@inbox.ru>
2022-08-22 22:41:06 +0600

    done

---
+++ Lesson_04/Task_027/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Greetings user! Let's count the summ of figures in your number");
+Console.Write("Enter your number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+int Summ (int arg)
+{
+while ( arg > 0)
+    {
+    int figure = arg % 10;
+    arg = arg / 10;
+    result = result + figure;
+    }
+return result;
+}
+Console.Write($"Your answer is {Summ(x)}.");
