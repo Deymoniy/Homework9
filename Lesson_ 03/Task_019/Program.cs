8a4aa106b9481270f68d18084e4bf925acdbe2dc
Yegor L <mmarf@inbox.ru>
2022-08-22 18:48:00 +0600

    initial commit

---
+++ Lesson_ 03/Task_019/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.WriteLine("Greetings user! Let's check if your 5-digit number is a palindrome");
+Console.Write("Enter your three - digit number: ");
+int input = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"So your number is {input}. Got it. Gimme a second to think.");
--- Task_013_Third_Figure/Program.cs
+++ Task_013_Third_Figure/Program.cs
@@ -3,5 +3,19 @@ Console.WriteLine("Greetings user! Let's check what is the third figure in your 
 Console.Write("Enter your number: ");
 int user = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"So your number is {user}. Got it. Gimme a second to think.");
-if (user > 100) Console.WriteLine($"The second figure is {user.ToString()[2] - '0'}.");
-else Console.WriteLine("Whooops! There isn't any in here");
+//if (user > 100) Console.WriteLine($"The second figure is {user.ToString()[2] - '0'}.");
+//else Console.WriteLine("Whooops! There isn't any in here");
+while ( user > 1000) && (A);
+{
+
+    user = user / 10;
+}
+return user;
+
+if (user < 1000)
+{
+int a = user / 10;
+int result = user - (a * 10);
+Console.WriteLine($"Your third figure is {result}");
+}
+else Console.WriteLine($"Your number {user} ain't got third figure");
