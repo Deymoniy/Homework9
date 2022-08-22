381008ec3916e103094429134d460e6a8981a430
Yegor L <mmarf@inbox.ru>
2022-08-22 20:02:28 +0600

    done

--- Lesson_ 03/Task_021/Program.cs
+++ Lesson_ 03/Task_021/Program.cs
@@ -2,21 +2,16 @@
 Console.WriteLine("Greetings user! Let's calculate the distance between two points in 3d.");
 Console.Write("Enter your first x: ");
 int x1 = Convert.ToInt32(Console.ReadLine());
-
 Console.Write("Enter your first y: ");
 int y1 = Convert.ToInt32(Console.ReadLine());
-
 Console.Write("Enter your first z: ");
 int z1 = Convert.ToInt32(Console.ReadLine());
-
 Console.WriteLine($"Your First dot is ({x1}, {y1}, {z1}). Moving to the next dot.");
 
 Console.Write("Enter your second x: ");
 int x2 = Convert.ToInt32(Console.ReadLine());
-
 Console.Write("Enter your second y: ");
 int y2 = Convert.ToInt32(Console.ReadLine());
-
 Console.Write("Enter your second z: ");
 int z2 = Convert.ToInt32(Console.ReadLine());
 
