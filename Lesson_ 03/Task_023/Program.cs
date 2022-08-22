bfde13f7c5ffc7c5723110caa5e68ed49a34d279
Yegor L <mmarf@inbox.ru>
2022-08-22 20:00:47 +0600

    done

--- Lesson_ 03/Task_023/Program.cs
+++ Lesson_ 03/Task_023/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Greetings user! Let's see all the number going before yours doubled.");
+Console.Write("Enter your number: ");
+int input = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"So your number is {input}. Got it. Gimme a second to think.");
+Console.Write("Your doubled numbers are: ");
+for(int i = 1; i <= input; i++)
+    {
+    int temp = i * i;
+    Console.Write($"{temp} ");
+    }
+Console.WriteLine(" ");
+Console.WriteLine($"All done.");
