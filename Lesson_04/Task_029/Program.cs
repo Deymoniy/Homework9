aafbe1b9e7017baea83a91e8dd457a7eeb65b31a
Yegor L <mmarf@inbox.ru>
2022-08-22 23:02:05 +0600

    done

--- Lesson_04/Task_029/Program.cs
+++ Lesson_04/Task_029/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Greetings user! My programm is going to create an array for you.");
+
+int[] array = new int [8];
+
+void FillArray(int[] arra)
+    {
+    int count = arra.Length;
+    for (int i = 0; i < count; i++)
+        {
+        arra[i] = new Random().Next(0, 100);
+        }
+    }
+    
+void PrintArray (int[] arra)
+    {
+    Console.Write("Here is your array: [ ");
+    int count = arra.Length;
+    for (int i = 0; i < count; i++) 
+        {
+        Console.Write($"{arra[i]} ");
+        }   
+    Console.Write("].");
+    }
+FillArray(array);
+PrintArray(array);
