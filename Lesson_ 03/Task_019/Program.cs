72fc8ff87c84252fdd20c51f457431325824f8b3
Yegor L <mmarf@inbox.ru>
2022-08-22 19:34:05 +0600

    done

--- Lesson_ 03/Task_019/Program.cs
+++ Lesson_ 03/Task_019/Program.cs
@@ -1,5 +1,21 @@
 ﻿Console.Clear();
 Console.WriteLine("Greetings user! Let's check if your 5-digit number is a palindrome");
-Console.Write("Enter your three - digit number: ");
+Console.Write("Enter your five - digit number: ");
 int input = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"So your number is {input}. Got it. Gimme a second to think.");
+
+if (input <10000)
+    {
+    Console.WriteLine("Wrong Number. You need 5-digit number!");    
+    }
+else if ((input % 10) == (input / 10000))
+    {
+    int temp1 = (input / 1000) % 10;
+    int temp2 = (input / 10) % 10;
+    if (temp1 == temp2)
+        {
+            Console.WriteLine("Yes,it is is a palindrome!");
+        }
+        else Console.WriteLine("No, it isn't is a palindrome!");
+    }
+else Console.WriteLine("No, it isn't a palindrome!");
