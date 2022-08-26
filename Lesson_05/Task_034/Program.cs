3684a0ba9c87744f706574d9b4a4ee7a1b7c8c05
Yegor L <mmarf@inbox.ru>
2022-08-26 17:00:20 +0600

    done

---
+++ Lesson_05/Task_034/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+Console.WriteLine("Greetings user! Let's see number of even-nembers in a random array.");
+
+int[] array = new int [8];
+
+void FillArray(int[] arra)
+    {
+    int count = arra.Length;
+    for (int i = 0; i < count; i++)
+        {
+        arra[i] = new Random().Next(99, 1000);
+        }
+    }
+
+void PrintArray (int[] arra)
+    {
+    Console.Write("Here is your array: [ ");
+    int count = arra.Length;
+    for (int i = 0; i < count; i++) 
+        {
+        Console.Write($"{arra[i]} ");
+        }   
+    Console.WriteLine("].");
+    }
+
+void Even(int[] arra)
+    {
+    int count = 0;
+   for (int i = 0; i < arra.Length; i++)
+   {
+    if (arra[i] % 2 == 0) count++;
+   }
+    Console.WriteLine($"The amount of even numbers is {count}.");
+    }
+FillArray(array);
+PrintArray(array);
+Even(array);
