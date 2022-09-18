2f938eecd7f1d7ae43cbcc94bed0e810e6632462
Yegor L <mmarf@inbox.ru>
2022-09-18 21:45:39 +0600

    Done

---
+++ Lesson_08/Task_060/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы 
+//каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+Console.Clear();
+Console.WriteLine("Greetings user! Let's see your 3D randomly generated array.");
+int[,,] matrix = new int[2, 2, 2];
+
+void FillArray(int[,,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = new Random().Next(10, 100);
+                Console.Write($"{array[i, j, k]} ");
+                Console.Write($" ({i}, {j}, {k}) ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray(matrix);
